b4cf0a18e962fc57604e9aa18a5d6c168a3b6bf4
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-05-12 01:09:52 -0700

    complete program

--- Precentage.java
+++ Precentage.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Precentage {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        String s = sc.nextLine().toLowerCase();
+        int c = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'c' || s.charAt(i) == 'g') {
+                c++;
+            } else {
+                continue;
+            }
+        }
+        double result = (double) c * 100 / n;
+        System.out.println(result);
     }
 }
